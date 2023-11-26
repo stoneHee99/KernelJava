0ffc2c30efe1dacf440353464ce58aec27c3f4f8
StoneHee99 <kiit0901@gmail.com>
2023-11-26 17:04:19 +0900

    add new spring project

---
+++ src/main/java/com/example/siverbellschool/SiverbellSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.siverbellschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiverbellSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiverbellSchoolApplication.class, args);
+    }
+
+}
